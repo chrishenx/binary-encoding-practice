d5cb3fb6c9b69a4d97411ee9e2bf6137001107fd
Chistian González <chrishendrix24@gmail.com>
2015-10-18 02:18:04 -0500

    The BinaryEncoder is able to generate clock signals

--- binaryencoder.cpp
+++ binaryencoder.cpp
@@ -1,3 +1,50 @@
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Christian González León
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+  */
+
 #include "binaryencoder.h"
 
+#include <QDebug>
 
+using namespace std;
+using namespace chrishenx;
+
+const double BinaryEncoder::DEFAULT_TRANS_SPEED = 1;
+const double BinaryEncoder::DEFAULT_AMPLITUDE = 5;
+
+BinaryEncoder::Data BinaryEncoder::generateClock()
+{
+    const int POINT_COUNT = mN * 4; // Four points for each bit
+    Data clock(POINT_COUNT);
+    const double f = mTransSpeed * 2; // Clock frecuency
+    const double T2 = 1.0 / f; // Clock period divided
+    double t = 0; // current time
+    for (int i = 0; i < POINT_COUNT; i += 4) {
+        clock[i] = make_pair(t, 0.0);
+        clock[i + 1] = make_pair(t += T2, 0.0);
+        clock[i + 2] = make_pair(t, mAmplitud);
+        clock[i + 3] = make_pair(t += T2, mAmplitud);
+    }
+    mTimeMax = t;
+    return clock;
+}
--- binaryencoder.h
+++ binaryencoder.h
@@ -1,18 +1,74 @@
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Christian González León
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+  */
+
 #ifndef BINARYENCODER_H
 #define BINARYENCODER_H
 
 #include <QVector>
-#include <QPair>
+#include <utility>
 
-class BinaryEncoder
-{
-public:
-    BinaryEncoder(QString valueToEncode)
-        : valueToEncode(valueToEncode) {}
+namespace chrishenx {
 
-    static QVector<QPair<double, double>> generateClock();
-private:
-    QString valueToEncode;
-};
+    class BinaryEncoder
+    {
+    public:
+        using Data = QVector<std::pair<double, double>>;
+
+        static const double DEFAULT_TRANS_SPEED; // In seconds
+        static const double DEFAULT_AMPLITUDE; // In volts
+
+        BinaryEncoder(QString valueToEncode)
+            : BinaryEncoder(valueToEncode, DEFAULT_TRANS_SPEED) {}
+
+        BinaryEncoder(QString valueToEncode, double transSpeed)
+                : BinaryEncoder(valueToEncode, transSpeed, DEFAULT_AMPLITUDE) {}
+
+        BinaryEncoder(QString valueToEncode, double transSpeed, double amplitude)
+                : mValueToEncode(valueToEncode), mTransSpeed(transSpeed),mAmplitud(amplitude) {
+            mN = valueToEncode.length();
+        }
+
+        Data generateClock();
+        Data generateTTL();
+        Data generateNRZL();
+        Data generateNRZI();
+        Data generateBipolar();
+        Data generatePseudoternary();
+        Data generateManchester();
+        Data generateDManchester();
+
+        double timeMax() const { return mLastTimeMax; }
+
+    private:
+        QString mValueToEncode;
+        double mTransSpeed; // Transmission speed
+        double mAmplitud; // Represent volts
+        double mTimeMax = 0;
+        int mN;
+    };
+
+} // chrishenx namespace end
+
 
 #endif // BINARYENCODER_H
